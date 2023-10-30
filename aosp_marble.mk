9a2539abd451368c0ba32d3b323bb1a6fd704539
Maulana Riaddy <maulriaddy@gmail.com>
2023-10-30 11:56:29 +0700

    marble: adaptable pixel star

--- aosp_marble.mk
+++ aosp_marble.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Awaken stuff.
-$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+# Inherit some common Pixel Star stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_SUPPORTS_QUICK_TAP := true
@@ -29,14 +29,16 @@ PRODUCT_NAME := awaken_marble
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # ADDED
-TARGET_FACE_UNLOCK_OPTOUT := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_ADAPTIVE_CHARGING := true
+PIXEL_LAUNCHER_VARIANT := glance13
 WITH_GAPPS := true
 
 # GAPPS
 WITH_GAPPS := true
 
-# FireDroid Maintainer Flags
-AWAKEN_MAINTAINER := SMGReborn x Maul's
+# PixelStar Maintainer Flags
+PIXELSTAR_MAINTAINER := SMGReborn x Mauls
 CUSTOM_BUILD_TYPE := UNOFFICIAL
