522399a4b6276afb2719104b77abb0965ae83385
Maulana Riaddy <maulriaddy@gmail.com>
2024-01-03 09:26:03 +0700

    marble: Bringup Afterlife

--- afterlife_marble.mk
+++ afterlife_marble.mk
@@ -9,20 +9,36 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
-
-EVO_BUILD_TYPE := OFFICIAL
-TARGET_BOOT_ANIMATION_RES := 1440
-TARGET_SUPPORTS_QUICK_TAP := true
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
 # Inherit from marble device.
 $(call inherit-product, device/xiaomi/marble/device.mk)
+
+# Build stuff.
+TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_FACE_UNLOCK_OPTOUT := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_ADAPTIVE_CHARGING := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+AFTERLIFE_GAPPS := true
+USE_PIXEL_CHARGING =: true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 ## Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := evolution_marble
+PRODUCT_NAME := afterlife_marble
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Nuke AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
+
+# GAPPS
+WITH_GAPPS := true
+
+# Maintainer
+AFTERLIFE_MAINTAINER := SMGReborn x Mauls
