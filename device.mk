fe7c4176a08e17ba8f747b371339dea62a3de961
someone5678 <nemui3353@gmail.com>
2024-01-16 15:32:37 +0100

    sweet: overlay: Add Bluetooth overlay
    * To enable A2DP support
    
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>

--- device.mk
+++ device.mk
@@ -475,6 +475,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     AospWifiResOverlayMarble \
     ApertureOverlayMarble \
+    BluetoothResMarble \
     CarrierConfigResMarble \
     FrameworksResCommon \
     FrameworksResOverlayMarble \
