1913141a6c3bd2ee281ebbe65d4a29aa2542f0d8
Maulana Riaddy <maulriaddy@gmail.com>
2024-03-19 10:20:17 +0700

    marble: Bringup Afterlife Tiramisu

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_marble.mk
+    $(LOCAL_DIR)/afterlife_marble.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_marble-eng \
-    evolution_marble-userdebug \
-    evolution_marble-user
+    afterlife_marble-eng \
+    afterlife_marble-userdebug \
+    afterlife_marble-user
--- afterlife_marble.mk
+++ afterlife_marble.mk
@@ -9,11 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
-EVO_BUILD_TYPE := OFFICIAL
+AFTERLIFE_GAPPS := true
 TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_ADAPTIVE_CHARGING := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
 # Inherit from marble device.
 $(call inherit-product, device/xiaomi/marble/device.mk)
@@ -22,7 +24,10 @@ $(call inherit-product, device/xiaomi/marble/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := evolution_marble
+PRODUCT_NAME := afterlife_marble
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Maintainer
+AFTERLIFE_MAINTAINER := SMGxMauls
